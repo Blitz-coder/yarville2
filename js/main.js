a89e8eb2dbfb8a0485fca341ee3ad7c0a7fc07ec
Blitz-coder <blitz-coder@narod.ru>
2021-02-27 01:44:11 +0700

    create: toggle button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
 
 
 
-});
+    function toggleMenu(){
+        var menuToggle = document.querySelector('.header-toggle');
+        // var menu = document.querySelector('.menu');
+        menuToggle.classList.toggle('header-toggle__active');
+        // menu.classList.toggle('active');
+    };
+
+
