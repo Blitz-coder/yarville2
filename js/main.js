8ecf5f0b2485fe1fb9ed7b704ee85ad4da9542a8
Blitz-coder <blitz-coder@narod.ru>
2021-03-03 00:35:35 +0700

    create: hero slider, custom dots

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 
+  function toggleMenu(){
+    var menuToggle = document.querySelector('.header-toggle');
+    var menu = document.querySelector('.navbar');
+    menuToggle.classList.toggle('header-toggle__active');
+    menu.classList.toggle('navbar-active');
+};
 
 
-    function toggleMenu(){
-        var menuToggle = document.querySelector('.header-toggle');
-        var menu = document.querySelector('.navbar');
-        menuToggle.classList.toggle('header-toggle__active');
-        menu.classList.toggle('navbar-active');
-    };
+
+$(document).ready(function () {
+
+    $('.hero-slider').slick({
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      pauseOnHover: false,
+      fade: true,
+      arrows: false,
+      dots: true,
+      dotsClass: 'hero-dots',
+      fadeSpeed: 1000,
+      lazyload: 'progressive'
+      });
+
+
+});
 
 
